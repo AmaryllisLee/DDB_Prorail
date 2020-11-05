9b1e86d3185c274258609c46d29719c617661214
DragonKiller952 <qjdegroot@gamil.com>
2020-11-05 18:01:22 +0100

    Dashboard devinitieve versie

--- dashboard/Backend.py
+++ dashboard/Backend.py
@@ -1,53 +1,69 @@
 from joblib import dump
 import pandas as pd
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 
-df = pd.read_csv('sap_storing_data_hu_project.csv', low_memory=False)
-df2 = df[['stm_oorz_groep', 'stm_oorz_code', 'stm_prioriteit', 'stm_fh_duur']].copy()
+# data inlezen en omzetten naar bruikbare vorm
+df = pd.read_csv('../Testing2/sap_storing_data_hu_project.csv', low_memory=False)
+df2 = df[['stm_oorz_groep', 'stm_oorz_code', 'stm_prioriteit', 'stm_contractgeb_mld', 'stm_contractgeb_gst',
+          'stm_sap_meldtijd', 'stm_aanntpl_dd', 'stm_aanntpl_tijd', 'stm_fh_ddt']].copy()
+df2['reparatietijd'] = (pd.to_datetime(df2['stm_fh_ddt']) -
+                        pd.to_datetime(df2['stm_aanntpl_dd'] + " " +
+                                       df2['stm_aanntpl_tijd'])).dt.total_seconds().div(60)
+df2 = df2[['stm_oorz_groep', 'stm_oorz_code', 'stm_prioriteit', 'stm_contractgeb_gst', 'stm_sap_meldtijd',
+           'reparatietijd']].copy()
 
-# Alle Nan waardes verwijderen
+# Alle Nan waardes, waardes die niet voorkomen in de csv bestanden en duplicaten verwijderen
 df2 = df2.dropna()
+df2 = df2[(df2['stm_contractgeb_gst'].isin([17, 50, 53, 54, 55, 56, 57, 62, 64, 82, 83])) == False]
 df2 = df2[(df2['stm_oorz_code'].isin([33, 48, 51, 999])) == False]
+df2 = df2[df2.duplicated(keep='last') == False]
 
-# Oorzaak groepen encoden naar numeric waardes mbv get_dummies . Explain one hote eencoding/get_Dummies
-df2 = pd.get_dummies(df2, columns=["stm_oorz_groep"])
+# Oorzaak groepen encoden naar numeric waardes mbv get_dummies
+df2 = pd.get_dummies(df2, columns=['stm_oorz_groep'])
+df2 = pd.get_dummies(df2, columns=['stm_oorz_code'])
+df2 = pd.get_dummies(df2, columns=['stm_contractgeb_gst'])
+
+# alle ongeldige meldtijden verwijderen en omzetten naar hele uren
+df2 = df2[df2['stm_sap_meldtijd'] != "::"]
+df2['stm_sap_meldtijd_uren'] = round(pd.to_timedelta(df2['stm_sap_meldtijd']).dt.total_seconds().div(3600)).astype(int)
+df2 = df2.drop('stm_sap_meldtijd', axis=1)
+df2['stm_sap_meldtijd_uren'] = df2['stm_sap_meldtijd_uren'].replace(24, 0)
+df2 = pd.get_dummies(df2, columns=['stm_sap_meldtijd_uren'])
 
 # Data selectie is een range is van 15 en 480.
-df2 = df2[(df2['stm_fh_duur'] >= 15) & (df2['stm_fh_duur'] <= 480)]
+df2 = df2[(df2['reparatietijd'] >= 15) & (df2['reparatietijd'] <= 480)]
 
-# data voor linregs voor marge
-df1 = df2[df2['stm_fh_duur'] >= df2['stm_fh_duur'].quantile(.75)]
-df3 = df2[df2['stm_fh_duur'] <= df2['stm_fh_duur'].quantile(.25)]
+# data voor divregs voor marge
+df1 = df2[df2['reparatietijd'] >= df2['reparatietijd'].quantile(.75)]
+df3 = df2[df2['reparatietijd'] <= df2['reparatietijd'].quantile(.25)]
 
-# define values
+# define features en targets
 
-features1 = df1[['stm_oorz_code', 'stm_prioriteit',
-                'stm_oorz_groep_ONR-DERD', 'stm_oorz_groep_ONR-RIB',
-                'stm_oorz_groep_TECHONV', 'stm_oorz_groep_WEER']]
-target1 = df1['stm_fh_duur']
+features1 = df1.drop('reparatietijd', axis=1)
+target1 = df1['reparatietijd']
 
-features2 = df2[['stm_oorz_code', 'stm_prioriteit',
-                'stm_oorz_groep_ONR-DERD', 'stm_oorz_groep_ONR-RIB',
-                'stm_oorz_groep_TECHONV', 'stm_oorz_groep_WEER']]
-target2 = df2['stm_fh_duur']
+features2 = df2.drop('reparatietijd', axis=1)
+target2 = df2['reparatietijd']
 
-features3 = df3[['stm_oorz_code', 'stm_prioriteit',
-                'stm_oorz_groep_ONR-DERD', 'stm_oorz_groep_ONR-RIB',
-                'stm_oorz_groep_TECHONV', 'stm_oorz_groep_WEER']]
-target3 = df3['stm_fh_duur']
+features3 = df3.drop('reparatietijd', axis=1)
+target3 = df3['reparatietijd']
 
 
 # Maak model
 
-linreg1 = LinearRegression()
-linreg2 = LinearRegression()
-linreg3 = LinearRegression()
+divreg1 = DecisionTreeRegressor()
+divreg2 = DecisionTreeRegressor()
+divreg3 = DecisionTreeRegressor()
 
 # fit model
-linreg1.fit(features1, target1)
-linreg2.fit(features2, target2)
-linreg3.fit(features3, target3)
+divreg1.fit(features1, target1)
+divreg2.fit(features2, target2)
+divreg3.fit(features3, target3)
 
-dump(linreg1, 'linreg1.joblib')
-dump(linreg2, 'linreg2.joblib')
-dump(linreg3, 'linreg3.joblib')
+# zet database en division tree om in files
+dump(divreg1, 'divreg1.joblib')
+dump(divreg2, 'divreg2.joblib')
+dump(divreg3, 'divreg3.joblib')
+
+dummiedf = pd.DataFrame(data=None, columns=features2.columns)
+dummiedf.to_pickle('dummiedf.pkl')
--- dashboard/Frontend.py
+++ dashboard/Frontend.py
@@ -1,45 +1,50 @@
-import numpy as np
+import pandas as pd
 from tkinter import *
 from tkinter import ttk
 from datetime import datetime
 from joblib import load
 
-linreg1 = load('linreg1.joblib')
-linreg2 = load('linreg2.joblib')
-linreg3 = load('linreg3.joblib')
+# externe database file en regression tree files importeren
+dummiedf = pd.read_pickle('dummiedf.pkl')
+divreg1 = load('divreg1.joblib')
+divreg2 = load('divreg2.joblib')
+divreg3 = load('divreg3.joblib')
 
 
-def get_predict(code, groep, prioriteit):  # voorspeld de hersteltijd aan de hand van de code, groep en prioriteit
-    if groep == 'ONR-DERD':
-        values = np.array([code, prioriteit, 1, 0, 0, 0]).reshape(1, -1)
-        result = str(linreg2.predict(values))
-        result = result.replace('[', '')
-        result = result.replace(']', '')
-        return float(result), values
-    elif groep == 'ONR-RIB':
-        values = np.array([code, prioriteit, 0, 1, 0, 0]).reshape(1, -1)
-        result = str(linreg2.predict(values))
-        result = result.replace('[', '')
-        result = result.replace(']', '')
-        return float(result), values
-    elif groep == 'TECHONV':
-        values = np.array([code, prioriteit, 0, 0, 1, 0]).reshape(1, -1)
-        result = str(linreg2.predict(values))
-        result = result.replace('[', '')
-        result = result.replace(']', '')
-        return float(result), values
-    elif groep == 'WEER':
-        values = np.array([code, prioriteit, 0, 0, 1, 0]).reshape(1, -1)
-        result = str(linreg2.predict(values))
-        result = result.replace('[', '')
-        result = result.replace(']', '')
-        return float(result), values
+def get_predict(code, groep, prioriteit, contract, tijd1, tijd2):  # voorspeld de hersteltijd aan de hand van de code, groep en prioriteit
+    # zet tijden om in uren en veranderd 24 uur naar 0 uur
+    if int(tijd2) >= 30:
+        tijd = int(tijd1)+1
+    else:
+        tijd = int(tijd1)
+
+    if tijd == 24:
+        tijd = 0
+
+    # maakt een dataframe aan om de dummies van de waardes te krijgen
+    df = pd.DataFrame(data=[[groep, float(code), int(prioriteit), float(contract), tijd]],
+                      columns=['stm_oorz_groep', 'stm_oorz_code', 'stm_prioriteit', 'stm_contractgeb_gst','stm_sap_meldtijd_uren'])
+    df = pd.get_dummies(df, columns=['stm_oorz_groep'])
+    df = pd.get_dummies(df, columns=['stm_oorz_code'])
+    df = pd.get_dummies(df, columns=['stm_contractgeb_gst'])
+    df = pd.get_dummies(df, columns=['stm_sap_meldtijd_uren'])
+
+    # alle lege waardes in de dummies aanvullen met 0
+    dummie = dummiedf.copy()
+    dummie = dummie.append(df)
+    dummie = dummie.fillna(0)
+
+    # prediction en de dummie dataframe returnen
+    result = str(divreg2.predict(dummie))
+    result = result.replace('[', '')
+    result = result.replace(']', '')
+    return float(result), dummie
 
 
 def get_accuracy(prediction, values):  # berekend de betrouwbaarheid van de voorspelde tijd aan de hand van marge
-    # van twee andere linregs
-    result1 = str(linreg1.predict(values))
-    result3 = str(linreg3.predict(values))
+    # van twee andere divregs
+    result1 = str(divreg1.predict(values))
+    result3 = str(divreg3.predict(values))
     result1 = result1.replace('[', '')
     result1 = result1.replace(']', '')
     result3 = result3.replace('[', '')
@@ -47,14 +52,13 @@ def get_accuracy(prediction, values):  # berekend de betrouwbaarheid van de voor
     result1 = float(result1)
     result3 = float(result3)
     marge = ((result1 - prediction)-(result3 - prediction))/2
-    # print(prediction, result1, result3, marge, marge/prediction*100)
 
     percentage = 100-(marge/prediction*100)
     # maakt van trust lager dan 0 een 0
     if percentage < 0:
         percentage = 0.0
 
-    return percentage
+    return percentage, marge
 
 
 def get_color(percent):  # geeft de kleurnaam aan de hand van het percentage
@@ -79,21 +83,27 @@ def get_input():  # Functie die de invoer van de comboboxen verwerkt en in de tr
     code = e1.get()
     groep = e2.get()
     prioriteit = e3.get()
+    contract = e4.get()
+    tijd1 = e5.get()
+    tijd2 = e6.get()
     # check of alle velden zijn ingevult voor dat hij het verwerkt
-    if code != "" and groep != "" and prioriteit != "":
+    if code != "" and groep != "" and prioriteit != "" and contract != "" and tijd1 != "" and tijd2 != "":
         # voorspelt de hersteltijd, berekend de betouwbaarheid en haalt de kleur van de regel op
-        prediction, values = get_predict(int(code), groep, int(prioriteit))
-        trust = get_accuracy(prediction, values)
+        prediction, values = get_predict(code, groep, prioriteit, contract, tijd1, tijd2)
+        trust, marge = get_accuracy(prediction, values)
         color = get_color(trust)
         # voegt alle waardes aan de treeview toe met een tijd van toevoeging
         tree.insert("", END, values=(
-            code, groep, prioriteit, round(prediction, 2), round(trust, 2), datetime.now().strftime("%d-%m-%Y %H:%M")),
+            code, groep, prioriteit, contract, (tijd1+":"+tijd2), round(prediction, 2), round(marge, 2), datetime.now().strftime("%d-%m-%Y %H:%M")),
                     tags=color)
 
         # reset de comboboxen
         e1.set('')
         e2.set('')
         e3.set('')
+        e4.set('')
+        e5.set('')
+        e6.set('')
 
 
 def remove():  # Functie die de geselecteerde row uit de treeview verwijderd
@@ -138,6 +148,9 @@ frame2 = Frame(master, background='black', borderwidth=2, relief='raised')
 Label(frame, text="Oorzaakscode", width=12).grid(row=0)
 Label(frame, text="Oorzaaksgroep", width=12).grid(row=1)
 Label(frame, text="Prioriteit", width=12).grid(row=2)
+Label(frame, text="Contractgebied", width=12).grid(row=3)
+Label(frame, text="Meldtijd", width=12).grid(row=4)
+Label(frame, text=":", width=1).grid(row=4, column=1)
 Label(frame2, text="Legenda", width=12, relief='raised').grid(row=10, column=2)
 Label(frame2, text="0% - 20%", width=12, background='red', relief='raised').grid(row=11, column=2)
 Label(frame2, text="21% - 40%", width=12, background='orange', relief='raised').grid(row=12, column=2)
@@ -150,6 +163,7 @@ Label(frame2, text="Not int", width=12, background='blue', relief='raised').grid
 # combobox voor de oorzaakscode
 e1 = ttk.Combobox(frame,
                   state="readonly",
+                  width=10,
                   values=[130, 131, 132, 133, 134, 135, 136, 140, 141,
                           142, 143, 144, 145, 146, 147, 148, 149, 150,
                           151, 152, 153, 154, 181, 182, 183, 184, 185,
@@ -159,14 +173,31 @@ e1 = ttk.Combobox(frame,
                           226, 227, 228, 229, 230, 231, 233, 234, 235,
                           239, 240, 241, 242, 250, 294, 298, 299])
 # combobox voor de oorzaaksgroep
-e2 = ttk.Combobox(frame, state="readonly", values=['ONR-RIB', 'ONR-DERD', 'TECHONV', 'WEER'])
+e2 = ttk.Combobox(frame, state="readonly", width=10, values=['ONR-RIB', 'ONR-DERD', 'TECHONV', 'WEER'])
 # combobox voor de prioriteit
-e3 = ttk.Combobox(frame, state="readonly", values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+e3 = ttk.Combobox(frame, state="readonly", width=10, values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# combobox voor het contractgebied
+e4 = ttk.Combobox(frame, state="readonly", width=10, values=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
+                                                             18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+                                                             32, 33, 34, 35, 36, 37, 51, 52, 58, 59, 60, 61, 63, 70,
+                                                             71, 81, 99])
+# combobox voor de meldtijd
+e5 = ttk.Combobox(frame, state="readonly", width=2, values=['00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
+                                                            '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+                                                            '20', '21', '22', '23'])
+
+e6 = ttk.Combobox(frame, state="readonly", width=2, values=['00', '01', '02', '03', '04', '05', '06', '07', '08', '09',
+                                                            '10', '11', '12', '13', '14', '15', '16', '17', '18', '19',
+                                                            '20', '21', '22', '23', '24', '25', '26', '27', '28', '29',
+                                                            '30', '31', '32', '33', '34', '35', '36', '37', '38', '39',
+                                                            '40', '41', '42', '43', '44', '45', '46', '47', '48', '49',
+                                                            '50', '51', '52', '53', '54', '55', '56', '57', '58', '59'])
+
 # button voor het invoeren van de comboboxen in de treeview
 b = Button(frame, width=12, text="Insert", command=get_input)
 
 # treeview aanmaken
-tree = ttk.Treeview(master)
+tree = ttk.Treeview(master, height=13)
 
 # button voor selectie uit de treeview halen
 btn = Button(master, width=12, text="Delete", command=remove)
@@ -176,7 +207,7 @@ style = ttk.Style()
 style.configure("Treeview.Heading", font=(None, 13))
 
 # columns in treeview aanmaken
-tree["columns"] = ["Oorzaakscode", "Oorzaaksgroep", "Prioriteit", "Voorspeltijd (min)", "Trust (%)", "Toevoegdatum"]
+tree["columns"] = ["Oorzaakscode", "Oorzaaksgroep", "Prioriteit", "Contractgebied", "Meldtijd",  "Voorspeltijd (min)", "Afwijking (min)", "Toevoegdatum"]
 tree["show"] = "headings"
 tree.heading("Oorzaakscode", text="Oorzaakscode",
              command=lambda col="Oorzaakscode": treeview_sort_column(tree, col, False))
@@ -186,11 +217,15 @@ tree.heading("Oorzaaksgroep", text="Oorzaaksgroep",
 tree.column("Oorzaaksgroep", minwidth=0, width=100)
 tree.heading("Prioriteit", text="Prioriteit", command=lambda col="Prioriteit": treeview_sort_column(tree, col, False))
 tree.column("Prioriteit", minwidth=0, width=100)
+tree.heading("Contractgebied", text="Contractgebied", command=lambda col="Contractgebied": treeview_sort_column(tree, col, False))
+tree.column("Contractgebied", minwidth=0, width=100)
+tree.heading("Meldtijd", text="Meldtijd", command=lambda col="Meldtijd": treeview_sort_column(tree, col, False))
+tree.column("Meldtijd", minwidth=0, width=100)
 tree.heading("Voorspeltijd (min)", text="Voorspeltijd (min)",
              command=lambda col="Voorspeltijd (min)": treeview_sort_column(tree, col, False))
 tree.column("Voorspeltijd (min)", minwidth=0, width=150)
-tree.heading("Trust (%)", text="Trust (%)", command=lambda col="Trust (%)": treeview_sort_column(tree, col, False))
-tree.column("Trust (%)", minwidth=0, width=100)
+tree.heading("Afwijking (min)", text="Afwijking (min)", command=lambda col="Afwijking (min)": treeview_sort_column(tree, col, False))
+tree.column("Afwijking (min)", minwidth=0, width=100)
 tree.heading("Toevoegdatum", text="Toevoegdatum",
              command=lambda col="Toevoegdatum": treeview_sort_column(tree, col, False))
 tree.column("Toevoegdatum", minwidth=0, width=150)
@@ -216,13 +251,16 @@ frame2.grid(row=8, column=0, rowspan=4, columnspan=2)
 e1.grid(row=0, column=1, padx=5, pady=5)
 e2.grid(row=1, column=1, padx=5, pady=5)
 e3.grid(row=2, column=1, padx=5, pady=5)
-b.grid(row=3, padx=5, pady=25)
-tree.grid(row=0, column=2, rowspan=8, columnspan=2)
+e4.grid(row=3, column=1, padx=5, pady=5)
+e5.grid(row=4, column=1, padx=5, pady=5, sticky=W)
+e6.grid(row=4, column=1, padx=5, pady=5, sticky=E)
+b.grid(row=7, padx=5, pady=25)
+tree.grid(row=0, column=2, rowspan=9, columnspan=2, sticky=N)
 btn.grid(row=10, column=3)
 
 # window settings
 master.configure(background='gray65')
-master.geometry('1075x310')
+# master.geometry('1075x310')
 master.resizable(False, False)
 
 mainloop()
